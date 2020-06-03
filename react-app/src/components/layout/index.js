701b60c61f3c1af4b174c5938783517c59a501d4
管显统 <1464245933@qq.com>
2020-06-03 23:57:03 +0800

    提交

---
+++ react-app/src/components/layout/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <div>
+                121
+            </div>
+        );
+    }
+}
+export default index;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './TodoList';
+import App from './components/layout';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
