c95fd1adf8a77dbc1b97cfda32383e82a41cc309
管显统 <1464245933@qq.com>
2020-06-04 00:56:36 +0800

    更新

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/layout';
+import 'antd/dist/antd.css';
+import './style/index.css';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   // 严格模式检查
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 // serviceWorker.unregister();
